587c0ce29f631879a76236076051ad0519a2b09e
jvsouzx <kreaction.god@gmail.com>
2025-03-25 10:05:16 -0300

    add queries.sh file in worldcup_db

--- students_db/insert_data.sh
+++ students_db/insert_data.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
-# Script to insert data from courses.csv and students.csv into students database
-
-PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
+PSQL="psql -X --username=USER --dbname=students --no-align --tuples-only -c"
 echo $($PSQL "TRUNCATE students, majors, courses, majors_courses")
 
 cat courses.csv | while IFS="," read MAJOR COURSE
--- worldcup_db/insert_data.sh
+++ worldcup_db/insert_data.sh
@@ -2,12 +2,10 @@
 
 if [[ $1 == "test" ]]
 then
-  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+  PSQL="psql --username=USER --dbname=worldcuptest -t --no-align -c"
 else
-  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+  PSQL="psql --username=USER --dbname=worldcup -t --no-align -c"
 fi
-
-# Do not change code above this line. Use the PSQL variable above to query your database.
 
 cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
 do
